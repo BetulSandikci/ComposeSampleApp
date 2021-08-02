a736cd463dd777faa48bcc54c8068c035d264a41
betulsandikci <betul.sandikci@d-teknoloji.com.tr>
2021-08-02 19:43:38 +0300

    compose cardView UI implemented

--- app/src/main/java/com/betuls/composesampleapppl/MainActivity.kt
+++ app/src/main/java/com/betuls/composesampleapppl/MainActivity.kt
@@ -3,49 +3,83 @@ package com.betuls.composesampleapppl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.kermit)
+            val description = "Kermit playing in the snow"
+            val title = "Kermit is playing in the snow"
+            Box(
                 modifier = Modifier
-                    .background(Color.Yellow)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .padding(top = 60.dp)
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-
-                //.requiredWidth(300.dp)
-
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
             ) {
-                Text(
-                    "Hello", modifier = Modifier
-                        .border(5.dp, Color.Cyan)
-                        .padding(15.dp)
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
                 )
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("World")
             }
+        }
+    }
+}
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(dimensionResource(R.dimen.margin_16)),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
     }
 }
